78e684724d2959847936eff29661ca1fe6d466fd
i5on9i <gaedduck@gmail.com>
2015-12-03 14:05:24 +0900

    - add rename_scaffold_app.py, this is better for Windows environment

---
+++ rename_scaffold_app.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+# Import the os module, for the os.walk function
+import os
+import sys
+
+
+POSTFIX = '.bak'
+
+
+def removeAllPys(dirName, fileList):
+    for fname in fileList:
+        os.remove(os.path.join(dirName, fname))
+
+
+def renameBak2Py(dirName, fileList):
+    for fname in fileList:
+        os.rename(os.path.join(dirName, fname + POSTFIX),
+                  os.path.join(dirName, fname))
+
+
+def main(argv):
+
+    projName = argv[1]
+
+    defaultName = 'scaffold'
+    # Set the directory you want to start from
+    rootDir = '.'
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        if dirName == os.path.join(rootDir, defaultName):
+            for fname in fileList:
+                print('\t%s' % fname)
+                with open(os.path.join(dirName, fname), 'r') as f:
+                    with open(os.path.join(dirName, fname + POSTFIX), 'w') as wf:
+                        for l in f:
+                            wf.write(l.replace(defaultName, projName))
+
+            removeAllPys(dirName, fileList)
+            renameBak2Py(dirName, fileList)
+            break
+
+
+if __name__ == '__main__':
+    main(sys.argv)
