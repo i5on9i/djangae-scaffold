116ea5a3660fdf303e26417b875288e8dedfe0b5
i5on9i <gaedduck@gmail.com>
2015-12-03 15:29:24 +0900

    - update the project name for root directory

--- rename_scaffold_app.py
+++ rename_scaffold_app.py
@@ -8,6 +8,14 @@ import sys
 POSTFIX = '.bak'
 
 
+def replaceNames(dirName, fileList, oldStr, newStr):
+    for fname in fileList:
+        with open(os.path.join(dirName, fname), 'r') as f:
+            with open(os.path.join(dirName, fname + POSTFIX), 'w') as wf:
+                for l in f:
+                    wf.write(l.replace(oldStr, newStr))
+
+
 def removeAllPys(dirName, fileList):
     for fname in fileList:
         os.remove(os.path.join(dirName, fname))
@@ -27,14 +35,13 @@ def main(argv):
     # Set the directory you want to start from
     rootDir = '.'
     for dirName, subdirList, fileList in os.walk(rootDir):
-        if dirName == os.path.join(rootDir, defaultName):
-            for fname in fileList:
-                print('\t%s' % fname)
-                with open(os.path.join(dirName, fname), 'r') as f:
-                    with open(os.path.join(dirName, fname + POSTFIX), 'w') as wf:
-                        for l in f:
-                            wf.write(l.replace(defaultName, projName))
+        if dirName == rootDir:
+            replaceNames(dirName, fileList, defaultName, projName)
+            removeAllPys(dirName, fileList)
+            renameBak2Py(dirName, fileList)
 
+        if dirName == os.path.join(rootDir, defaultName):
+            replaceNames(dirName, fileList, defaultName, projName)
             removeAllPys(dirName, fileList)
             renameBak2Py(dirName, fileList)
             os.rename(os.path.join(rootDir, defaultName),
